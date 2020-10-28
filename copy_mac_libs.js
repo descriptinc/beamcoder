ed0ff8d53ddf219d06f68aff88ac25175119c391
Marcello Bastéa-Forte <marcello@descript.com>
2020-10-28 08:19:03 -0700

    update copy_mac_libs to recurse

--- copy_mac_libs.js
+++ copy_mac_libs.js
@@ -1,11 +1,41 @@
+const { resolve, basename } = require('path');
 const { execSync } = require('child_process');
-  const lines = execSync('otool -L build/Release/beamcoder.node').toString('utf8').split('\n');
+const { existsSync } = require('fs');
+
+function logAndExec(cmd) {
+  console.log(`EXEC ${cmd}`);
+  execSync(cmd);
+}
+
+const baseDir = resolve(__dirname, 'build/Release');
+
+function copyDylibs(binaryName) {
+  const binaryPath = resolve(baseDir, binaryName);
+  console.log(`Inspecting ${binaryPath}`);
+  const lines = execSync(`otool -L ${binaryPath}`).toString('utf8').split('\n');
+  const libs = [];
   for (const line of lines) {
-    const match = /\s*(\S*\/ffmpeg\/\S*\/([^/]+\.dylib))/.exec(line);
+    // Only operate on dylibs in /usr/local
+    const match = /\/usr\/local\/\S+\.dylib/.exec(line);
     if (match) {
-      const [, path, filename] = match;
-      console.log(`Rewriting ${path} to @rpath/${filename}`);
-      execSync(`cp ${path} build/Release/${filename}`);
-      execSync(`install_name_tool -change ${path} @rpath/${filename} build/Release/beamcoder.node`);
+      const [path] = match;
+      const filename = basename(path);
+      const newFilename = resolve(baseDir, filename);
+      if (!existsSync(newFilename)) {
+        logAndExec(`cp ${path} ${newFilename}`);
+        logAndExec(`chmod u+w ${newFilename}`);
+        copyDylibs(filename);
+      } else {
+        console.log(`[DEBUG] skipping file because it already exists: ${newFilename}`);
+      }
+      libs.push({ path, filename });
+    } else {
+      console.log(`[DEBUG] skipping otool output: ${line}`);
     }
   }
+  if (libs.length > 0) {
+    logAndExec(`install_name_tool ${libs.map(({path, filename}) => `-change ${path} @rpath/${filename}`).join(' ')} ${binaryPath}`);
+  }
+}
+
+copyDylibs('beamcoder.node');
