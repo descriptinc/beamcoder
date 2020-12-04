d952d44f5c94d775635648d8a4c623635bd4acfe
Steve Rubin <nerdforlife@gmail.com>
2020-12-04 09:36:07 -0800

    paths entry point

--- index.js
+++ index.js
@@ -19,8 +19,6 @@
   14 Ormiscaig, Aultbea, Achnasheen, IV22 2JJ  U.K.
 */
 
-const os = require('os');
-const path = require('path');
 const beamcoder = require('bindings')('beamcoder');
 const beamstreams = require('./beamstreams.js');
 
@@ -42,33 +40,5 @@ beamcoder.muxerStream = beamstreams.muxerStream;
 
 beamcoder.makeSources = beamstreams.makeSources;
 beamcoder.makeStreams = beamstreams.makeStreams;
-
-function getPath(name) {
-  // TODO: handle different platforms and architectures
-
-  // const binaries = Object.assign(Object.create(null), {
-  //   darwin: ['x64'],
-  //   linux: ['x64', 'ia32', 'arm64', 'arm'],
-  //   win32: ['x64', 'ia32']
-  // });
-
-  const platform = process.env.npm_config_platform || os.platform();
-
-  // const arch = process.env.npm_config_arch || os.arch();
-  //
-  // if (!binaries[platform] || binaries[platform].indexOf(arch) === -1) {
-  //   return null;
-  // }
-
-  return path.join(
-    __dirname,
-    'build',
-    'Release',
-    platform === 'win32' ? name + '.exe' : name
-  );
-}
-
-beamcoder.ffmpegPath = getPath('ffmpeg');
-beamcoder.ffprobePath = getPath('ffprobe');
 
 module.exports = beamcoder;
