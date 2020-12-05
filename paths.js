45928fa71d68b9f56459a68d149bbe1b83c88838
Steve Rubin <nerdforlife@gmail.com>
2020-12-04 16:15:22 -0800

    Update paths.js

--- paths.js
+++ paths.js
@@ -23,21 +23,19 @@ const path = require('path');
 const os = require('os');
 
 function getPath(name) {
-  // TODO: handle different platforms and architectures
-
-  // const binaries = Object.assign(Object.create(null), {
-  //   darwin: ['x64'],
-  //   linux: ['x64', 'ia32', 'arm64', 'arm'],
-  //   win32: ['x64', 'ia32']
-  // });
+  const binaries = Object.assign(Object.create(null), {
+    darwin: ['x64'],
+    linux: ['x64', 'ia32', 'arm64', 'arm'],
+    win32: ['x64', 'ia32']
+  });
 
   const platform = process.env.npm_config_platform || os.platform();
 
-  // const arch = process.env.npm_config_arch || os.arch();
-  //
-  // if (!binaries[platform] || binaries[platform].indexOf(arch) === -1) {
-  //   return null;
-  // }
+  const arch = process.env.npm_config_arch || os.arch();
+
+  if (!binaries[platform] || binaries[platform].indexOf(arch) === -1) {
+    return null;
+  }
 
   return path.join(
     __dirname,
