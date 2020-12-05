6c3278906362998a6df78e75add9f8d17e97c8dc
Steve Rubin <nerdforlife@gmail.com>
2020-12-04 16:09:47 -0800

    cleanup

--- check_libs.sh
+++
@@ -1,7 +0,0 @@
-#for file in prebuilds/build/Release/*
-for file in /Users/srubin/code/descript-web-v2/node_modules/beamcoder/build/Release/*
-do
-  echo $file
-  otool -l $file | grep -A 3  LC_VERSION_MIN_MACOSX
-  otool -l $file | grep -A 7  LC_BUILD_VERSION
-done
